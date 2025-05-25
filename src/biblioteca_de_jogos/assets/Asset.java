4b47724e5c16fb1c0fe3b13c16f2f28726d3e0dc
Rynilan <ilanvitorsantos@gmail.com>
2025-05-25 13:09:21 -0300

    raw: ajustadas as estruturas de pastas e pacotes.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

---
+++ src/biblioteca_de_jogos/assets/Asset.java
@@ -0,0 +1,23 @@
+package biblioteca_de_jogos.assets;
+
+/**
+ * Só um arquivo para criar a pasta no repositório
+ * do GitHub e um exemplo chocho de classe (não mexa
+ * se não tem porquê).
+ */ 	
+public class Asset {
+
+	/** Um atributo. */
+	private String atributo;
+
+	/** Um construtor. */
+	public Asset() {
+		this.atributo = "Atributo";
+	}
+
+	/** Um método */
+	public void metodo() {
+		this.atributo.isEmpty();
+	}
+
+} 	
