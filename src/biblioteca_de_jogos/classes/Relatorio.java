cc158290e79a8d4d32fb88d194c2c3004cc3e704
CauandoP <148398651+peludo99@users.noreply.github.com>
2025-05-31 15:21:41 -0300

    Adicao de Classes "Relatorio" e "Usuario".

---
+++ src/biblioteca_de_jogos/classes/Relatorio.java
@@ -0,0 +1,11 @@
+package biblioteca_de_jogos.classes;
+import java.time.LocalDate;
+
+public class Relatorio {
+
+    private int  id;
+    public String tipo;
+    public LocalDate dataGeracao;
+
+
+}
---
+++ src/biblioteca_de_jogos/classes/Usuario.java
@@ -0,0 +1,32 @@
+package biblioteca_de_jogos.classes;
+
+public class Usuario {
+
+
+    private int id;
+    public String nome;
+    public String email;
+    public String telefone;
+    public String status;
+
+
+
+
+    public boolean podeEmprestar()
+    {
+        return false; // alteravel
+    }
+
+    public boolean temDevolucoesAtrasadas()
+    {
+        return false; // alteravel 
+    }
+
+
+
+
+
+
+
+    
+}
