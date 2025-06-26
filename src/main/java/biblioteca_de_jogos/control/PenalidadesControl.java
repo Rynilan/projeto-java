3eb7ce0667e663f3d9ac15b6f7fb44caddf91206
Rynilan <ilanvitorsantos@gmail.com>
2025-06-26 01:23:58 -0300

    code: separado o model e controler das penalidades.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- src/main/java/biblioteca_de_jogos/control/PenalidadesControl.java
+++ src/main/java/biblioteca_de_jogos/control/PenalidadesControl.java
@@ -1,32 +1,41 @@
-package biblioteca_de_jogos.assets;
+package biblioteca_de_jogos.control;
 
 import java.util.List;
 import java.util.ArrayList;
 
 import biblioteca_de_jogos.classes.Penalidade;
 import biblioteca_de_jogos.classes.Emprestimo;
+import biblioteca_de_jogos.model.Penalidades;
 
 
-public class Penalidades {
+public class PenalidadesControl {
 
-	private List<Penalidade> penalidades;
+	private static PenalidadesControl self;
+	private Penalidades penalidades;
 	private int id;
 
-	public Penalidades() {
-		this.penalidades = new ArrayList<Penalidade>();
+	private PenalidadesControl() {
+		this.penalidades = Penalidades.getInstance();
 		this.id = -1;
+	}
+
+	public static PenalidadesControl getInstance() {
+		if (PenalidadesControl.self == null) {
+			PenalidadesControl.self = new PenalidadesControl();
+		}
+		return PenalidadesControl.self;
 	}
 
 	public void criarPenalidade(Emprestimo emprestimo) {
 		this.id++;
-		this.penalidades.add(
+		this.penalidades.getPenalidades().add(
 			new Penalidade(this.id, String.format("Usuário nº%i atraso da entrega de um jogo.", emprestimo.getIdUsuario()), "bloqueio", emprestimo.getIdUsuario(), emprestimo)
 		);
 	}
 
 	public List<Penalidade> penalidadesDeUsuario(int idUsuario) {
 		List<Penalidade> achadas = new ArrayList<Penalidade>();
-		for (Penalidade penalidade: this.penalidades) {
+		for (Penalidade penalidade: this.penalidades.getPenalidades()) {
 			if (penalidade.getIdUsuario() == idUsuario) {
 				achadas.add(penalidade);
 			}
---
+++ src/main/java/biblioteca_de_jogos/model/Penalidades.java
@@ -0,0 +1,26 @@
+package biblioteca_de_jogos.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import biblioteca_de_jogos.classes.Penalidade;
+
+public class Penalidades {
+	private static Penalidades self;
+	private List<Penalidade> penalidades;
+
+	private Penalidades() {
+		this.penalidades = new ArrayList<Penalidade>();
+	}
+
+	public static Penalidades getInstance() {
+		if (Penalidades.self == null) {
+			Penalidades.self = new Penalidades();
+		}
+		return Penalidades.self;
+	}
+
+	public List<Penalidade> getPenalidades() {
+		return this.penalidades;
+	}
+}
